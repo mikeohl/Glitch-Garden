020093e58ad80ff38235c0a8b402c662a09e8b8c
mikeohl <mohl@live.com>
2017-12-12 02:59:19 -0800

    Created Script for spawning attackers

---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackerSpawner : MonoBehaviour {
+
+	public GameObject [] attackerPrefabArray;
+	
+	// private float lastSpawnTime;
+	private Attacker attacker;
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject thisAttacker in attackerPrefabArray) {
+			if (isTimeToSpawn (thisAttacker)) {
+				// lastSpawnTime = Time.time;
+				Spawn (thisAttacker);
+			}
+		}
+	}
+	
+	bool isTimeToSpawn (GameObject thisAttacker) {
+		attacker = thisAttacker.GetComponent<Attacker>();
+		float spawnTime = attacker.spawnTime;
+		float spawnsPerSecond = 1 / spawnTime;
+		
+		if (Time.deltaTime > spawnTime) {
+			Debug.LogWarning ("Spawn rate capped by frame rate");
+		}
+		
+		float spawnsPerFrame = spawnsPerSecond * Time.deltaTime;
+		
+		if (Random.value < spawnsPerFrame) {
+			return true;
+		}
+		
+		// if (Time.time - lastSpawnTime >= attacker.spawnRate) {
+		//	return true; 
+		// }
+		return false;
+	}
+	
+	void Spawn (GameObject attacker) {
+		GameObject newAttacker = Instantiate (attacker) as GameObject;
+		newAttacker.transform.parent = this.transform;
+		newAttacker.transform.position = this.transform.position;
+	}
+}
