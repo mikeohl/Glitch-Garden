614d84f478d33831c6ac6ecd3765f1e8b1f053f0
mikeohl <mohl@live.com>
2017-12-14 03:13:01 -0800

    Added slider scrip to represent game timer

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour {
+
+	public float levelTimeInSeconds = 100;
+	
+	private Slider slider;
+	private LevelManager levelmanager;
+	private AudioSource audioSource;
+	private bool levelComplete = false;
+	private GameObject winLabel;
+	
+
+	// Use this for initialization
+	void Start () {
+		slider = GetComponent<Slider>();
+		audioSource = GetComponent<AudioSource>();
+		levelmanager = GameObject.FindObjectOfType<LevelManager>();
+		winLabel = GameObject.Find("You Won");
+		if (!winLabel) {
+			Debug.LogWarning("No Win UI Text for victory"); 
+		} else {
+			winLabel.SetActive(false);
+		}
+		
+		slider.value = slider.minValue =  0;
+		slider.maxValue = levelTimeInSeconds;
+	}
+	
+	// Update is called once per frame
+	// TODO: Fix instance where enemy hits lose collider while victory music is playing causing loss.
+	void Update () {
+		slider.value += Time.deltaTime;
+		if (slider.value >= levelTimeInSeconds && !levelComplete) {
+			audioSource.Play();
+			winLabel.SetActive(true);
+			Invoke ("LoadNextLevel", audioSource.clip.length);
+			levelComplete = true;
+		}
+	}
+	
+	void LoadNextLevel () {
+		levelmanager.LoadNextLevel();
+	}
+}
