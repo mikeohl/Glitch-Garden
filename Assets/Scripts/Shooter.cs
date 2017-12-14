6d55d2df748cb03026899d0d8f412e1f27073e5e
mikeohl <mohl@live.com>
2017-12-13 19:09:07 -0800

    Added method to shoot attacker if ahead in lane

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -6,9 +6,15 @@ public class Shooter : MonoBehaviour {
 	public GameObject projectile, launcher;
 	
 	private GameObject projectileParent;
+	private Animator animator;
+	private AttackerSpawner laneSpawner;
 	
 	// Use this for initialization
 	void Start () {
+		animator = GetComponent<Animator>();
+		SetLaneSpawner ();
+		
+		// Create Parent for defenders if necessary
 		projectileParent = GameObject.Find ("Projectiles");
 		
 		if (!projectileParent) {
@@ -18,7 +24,41 @@ public class Shooter : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (IsAttackerAheadInLane()) {
+			animator.SetBool ("isAttacking", true);
+		} else {
+			animator.SetBool ("isAttacking", false);
+		}
+	}
 	
+	// Find spawner in same lane as shooting defender
+	void SetLaneSpawner () {
+		AttackerSpawner [] spawners = GameObject.FindObjectsOfType<AttackerSpawner>();
+		foreach (AttackerSpawner spawner in spawners) {
+			if (spawner.transform.position.y == this.transform.position.y) {
+				laneSpawner = spawner;
+				return;
+			}
+		}
+		
+		Debug.LogError ("Lane Spawner not found");
+	}
+	
+	bool IsAttackerAheadInLane() {
+		// Check if there are any attackers
+		if (laneSpawner.transform.childCount <= 0) {
+			return false;
+		}
+		
+		// Check if any attacker is in front of a defender in the lane
+		foreach (Transform attacker in laneSpawner.transform) {
+			if (this.transform.position.x <= attacker.transform.position.x) {
+				return true;
+			}
+		}
+		
+		// Attacker(s) not in from of Defender
+		return false; 
 	}
 	
 	private void Fire () {
