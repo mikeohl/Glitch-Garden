144e918bebf4c78cb440b5ef6075bfd61a10d426
mikeohl <mohl@live.com>
2017-12-13 19:02:58 -0800

    Script to Display Star Current

---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class StarDisplay : MonoBehaviour {
+
+	public enum Status {FAILURE, SUCCESS};
+
+	private int stars = 100;
+	private Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text>();
+		UpdateDisplay ();
+		
+		// stars = GameObject.FindObjectOfType<Stars>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void AddStars (int amount) {
+		stars += amount;
+		UpdateDisplay ();
+		// print (amount + " stars added to display");
+	}
+	
+	public Status UseStars (int amount) {
+		if (stars >= amount) {
+			stars -= amount;
+			UpdateDisplay ();
+			return Status.SUCCESS;
+		}
+		return Status.FAILURE;
+	}
+	
+	public void UpdateDisplay () {
+		text.text = stars.ToString ();
+	}
+}
