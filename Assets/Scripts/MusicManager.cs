95401c95f9cd9ed45aaf881f0753abc63fadbff7
mikeohl <mohl@live.com>
2017-12-15 02:50:46 -0800

    Added set volume in start

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -15,6 +15,7 @@ public class MusicManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		audioSource = GetComponent<AudioSource>();
+		audioSource.volume = PlayerPrefsManager.GetMasterVolume();
 	}
 	
 	void OnLevelWasLoaded (int level) {
