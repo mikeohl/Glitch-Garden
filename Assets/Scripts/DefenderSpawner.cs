ecd9ebdc7ab9a54824ad9f4e4fcc06c891fb66b0
mikeohl <mikeohl@live.com>
2018-03-04 19:05:35 -0800

    Added if conditional in SpawnDefender
    Prevents attempt to spawn defender when no defender is selected

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -1,5 +1,6 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/* DefenderSpawner handles placement of defender in grid on user mouse click. */
+
+using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour {
 
@@ -10,42 +11,43 @@ public class DefenderSpawner : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+
+        // Group defenders in defender GameObject
 		defenderParent = GameObject.Find("Defenders");
-		
 		if (!defenderParent) {
 			defenderParent = new GameObject("Defenders");
 		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
-	void SpawnDefender ()
-	{
-		GameObject newDefender = Instantiate (Button.selectedDefender, SnapToGrid (MouseClickToWorldPoint ()), Quaternion.identity) as GameObject;
-		newDefender.transform.parent = defenderParent.transform;
-		// Debug.Log (SnapToGrid (MouseClickToWorldPoint ()));
+    // Call SpawnDefender for selected defender on player mouse click
+    void OnMouseDown() {
+        if (SelectDefenderButton.selectedDefender) {
+            int starCost = SelectDefenderButton.selectedDefender.GetComponent<DefenderCurrency>().starCost;
+            if (starDisplay.UseStars(starCost) == StarDisplay.Status.SUCCESS) {
+                SpawnDefender();
+            } else {
+                Debug.Log("Insufficient Stars");
+            }
+        }
+    }
+
+    // Place selected defender on board
+    private void SpawnDefender () {
+        if (SelectDefenderButton.selectedDefender) {
+            GameObject newDefender = Instantiate(SelectDefenderButton.selectedDefender, 
+                                                 SnapToGrid(MouseClickToWorldPoint()), 
+                                                 Quaternion.identity) as GameObject;
+            newDefender.transform.parent = defenderParent.transform;
+        }
 	}
 	
-	void OnMouseDown () {
-		// Debug.Log ("mouse clicked");
-		if (starDisplay.UseStars(Button.selectedDefender.GetComponent<Defender>().starCost) == StarDisplay.Status.SUCCESS) {
-			SpawnDefender ();
-		} else {
-			Debug.Log ("Insufficient Stars");
-		}
-		
-	}
-	
-	Vector2 SnapToGrid (Vector2 worldPoint) {
+    // Set position of defender to center of cell
+	private Vector2 SnapToGrid (Vector2 worldPoint) {
 		return new Vector2 (Mathf.RoundToInt(worldPoint.x), Mathf.RoundToInt(worldPoint.y));
 	}
 	
-	Vector2 MouseClickToWorldPoint () {
-		// Arbitrary z-distance from camera 10f
-		
+	private Vector2 MouseClickToWorldPoint () {
+		// 10.0f in mousePosition vector3 is arbitrary z-distance from camera
 		myCamera = GameObject.FindObjectOfType<Camera>();
 		Vector3 mousePosition = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 10.0f);
 		return myCamera.ScreenToWorldPoint(mousePosition);
