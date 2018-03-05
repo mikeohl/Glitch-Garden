9702aed868487a730b9af44334df9ebbc4e253ad
mikeohl <mikeohl@live.com>
2018-03-04 18:02:05 -0800

    Refactored and added comments

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -1,5 +1,8 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/* AttackSpawner spawns attackers into the game with check to 
+ * IsTimeToSpawn and Spawn functions 
+ */
+
+using UnityEngine;
 
 public class AttackerSpawner : MonoBehaviour {
 
@@ -9,30 +12,33 @@ public class AttackerSpawner : MonoBehaviour {
 	private Attacker attacker;
 	
 	// Update is called once per frame
+    // Check if a new attacker should be spawned
 	void Update () {
 		foreach (GameObject thisAttacker in attackerPrefabArray) {
-			if (isTimeToSpawn (thisAttacker)) {
-				// lastSpawnTime = Time.time;
+			if (IsTimeToSpawn (thisAttacker)) {
 				Spawn (thisAttacker);
 			}
 		}
 	}
-	
-	bool isTimeToSpawn (GameObject thisAttacker) {
+
+    // Check if an enemy should be spawned according to attacker's 
+    // spawn time frequency. [Randomized about spawn frequency]
+	private bool IsTimeToSpawn (GameObject thisAttacker) {
 		attacker = thisAttacker.GetComponent<Attacker>();
-		float spawnTime = attacker.spawnTime;
-		float spawnsPerSecond = 1 / spawnTime;
-		
-		if (Time.deltaTime > spawnTime) {
-			Debug.LogWarning ("Spawn rate capped by frame rate");
-		}
-		
+
+        // Log warning if attacker spawn time exceeds frame rate
+        if (Time.deltaTime > attacker.spawnTime) {
+            Debug.LogWarning("Spawn rate capped by frame rate");
+        }
+
+        float spawnsPerSecond = 1 / attacker.spawnTime;
 		float spawnsPerFrame = spawnsPerSecond * Time.deltaTime;
 		
+        // Check against random value to randomize around frequency
 		return Random.value < spawnsPerFrame;
 	}
 	
-	void Spawn (GameObject attacker) {
+	private void Spawn (GameObject attacker) {
 		GameObject newAttacker = Instantiate (attacker) as GameObject;
 		newAttacker.transform.parent = this.transform;
 		newAttacker.transform.position = this.transform.position;
