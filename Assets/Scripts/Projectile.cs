f8c6b127bce24a7ac5646f3cd3c023164f83e119
mikeohl <mikeohl@live.com>
2018-03-04 20:55:16 -0800

    Added comments

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,17 +1,22 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/* Projectile controls the speed and damage of projectiles fired at
+ * attackers by the defenders. The projectile deals damage and then
+ * is destroyed on collision.
+ */
+
+using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
-	public float speed, damage;
+    public float speed, damage;
 	
 	// Update is called once per frame
+    // Move projectile each frame
 	void Update () {
 		transform.Translate (Vector3.right * speed * Time.deltaTime);
 	}
 	
+    // Deal damage to attacker on collision and then destroy projectile
 	void OnTriggerEnter2D (Collider2D collider) {
-		// Debug.Log (name + " collided with " + collider); 
 		
 		GameObject attacker = collider.gameObject;
 		
