1ef54e25d1ac4f46aab46c68bae5d95f987f6777
mikeohl <mikeohl@live.com>
2018-03-04 17:47:47 -0800

    Added comments

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -1,55 +1,48 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/* Attacker.cs controls movement speed of the attacker at the 
+ * defender, setting the current target, and setting animation
+ * boolean trigger for attack animation.
+ */
+
+using UnityEngine;
 
 [RequireComponent (typeof (Rigidbody2D))]
 public class Attacker : MonoBehaviour {
 
 	[Tooltip ("Average number of seconds between appearances")]
 	public float spawnTime;
-	// [Range (-1.0f, 1.5f)]
-	public float speed;
-	
-	private GameObject currentTarget;
+	public float speed;     // [Suggested Range (-1.0f, 1.5f)]
+
+    private GameObject currentTarget;
 	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
-		// Rigidbody2D myRigidbody = gameObject.AddComponent<Rigidbody2D>();
-		// myRigidbody.isKinematic = true;
-		
-		//defenderParent = GameObject.Find("Defenders");
-		
-		//if (!defenderParent) {
-		//	defenderParent = new GameObject("Defenders");
-		//}
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    // Move attacker across the screen and set animation trigger if 
+    // attacker has a target
+    void Update () {
 		transform.Translate (Vector3.left * speed * Time.deltaTime);
 		if (!currentTarget) {
 			animator.SetBool("isAttacking", false);
 		}
 	}
-	
-	void OnTriggerEnter2D () {
-		// Debug.Log (name + "Trigger Enter"); 
-	}
-	
-	public void SetSpeed (float speed) {
-		this.speed = speed;
-	}
-	
+
 	// Called from the animator at the time of the actual attack
 	public void StrikeCurrentTarget (float damage) {
 		if (currentTarget) {
-			// Debug.Log (name + "doing " + damage + " damage.");
 			currentTarget.GetComponent<Health>().DealDamage(damage);
 		}	
 	}
 	
+    // Set defender object as current target for attacker
 	public void Attack (GameObject obj) {
 		currentTarget = obj;
 	}
+
+    public void SetSpeed(float speed) {
+        this.speed = speed;
+    }
 }
