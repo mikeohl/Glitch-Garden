d9a4973172dce35de5b7bbda6df1da9f78552dc1
mikeohl <mikeohl@live.com>
2018-03-04 18:52:28 -0800

    Renamed script and added comments

---
+++ Assets/Scripts/DefenderCurrency.cs
@@ -0,0 +1,22 @@
+﻿/* DefenderCurrency allows Unity editor to set the star cost
+ * of each defender. Defender can also add stars to the 
+ * total in StarDisplay.
+ */
+
+using UnityEngine;
+
+public class DefenderCurrency : MonoBehaviour {
+
+	public int starCost = 10; // default, change in editor
+
+	private StarDisplay starDisplay;
+
+	// Use this for initialization
+	void Start () {
+		starDisplay = GameObject.FindObjectOfType<StarDisplay>();
+	}
+	
+	public void AddStars (int amount) {
+		starDisplay.AddStars (amount);
+	}
+}
